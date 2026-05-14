c4d2d55b03a67c350a951eedae8e02f9eb20fd53
oswin902m <1665434994@qq.com>
2026-05-14 17:08:07 +0800

    shadcn setup, dark mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,8 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import appCss from '../styles.css?url'
+import Navbar from '@/components/web/navbar'
+import { ThemeProvider } from '@/components/web/theme-provider'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -22,21 +24,24 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <HeadContent />
       </head>
       <body>
-        {children}
-        <TanStackDevtools
-          config={{ position: 'bottom-right' }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
+        <ThemeProvider defaultTheme="system" storageKey="theme">
+          <Navbar />
+          {children}
+          <TanStackDevtools
+            config={{ position: 'bottom-right' }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+        </ThemeProvider>
         <Scripts />
       </body>
     </html>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
+import { ChartAreaInteractive } from '@/components/chart-area-interactive'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
+    <>
+      <ChartAreaInteractive />
+      <ChartAreaInteractive />
+      <ChartAreaInteractive />
+    </>
   )
 }
